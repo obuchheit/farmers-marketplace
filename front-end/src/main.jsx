0b0e9e257720f5194b2c9dadab8971273f19f8bf
obuchheit <obuchheit45@gmail.com>
2024-12-09 14:30:22 -0600

    Add react skeleton with router

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
 import './index.css'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
