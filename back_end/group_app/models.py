37d3ad1e787086d0e429ce8f634ddd4d438bd542
obuchheit <obuchheit45@gmail.com>
2024-12-27 21:27:03 -0600

    Add invitation model

--- back_end/group_app/models.py
+++ back_end/group_app/models.py
@@ -64,3 +64,18 @@ class JoinRequest(models.Model):
 
     def __str__(self):
         return f"Request by {self.user} to join {self.group} - Approved: {self.is_approved}"
+    
+
+class Invitation(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="invitations")
+    invited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_invitaitons")
+    invitee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recieved_invitations")
+    staus = models.CharField(
+        max_length=10,
+        choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected')],
+        default='pending'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('group', 'invitee')
